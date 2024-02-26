934971cf85c5754e8670df1f4a01b8c4246fec11
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-26 14:23:27 +0530

    Added promise

--- interview_questions/promise.js
+++ interview_questions/promise.js
@@ -10,3 +10,9 @@ console.log("C");
 Promise.resolve().then(() => console.log("D"));
 
 console.log("hello");
+
+// output => a C D B
+// reason => js has micro task queue and callback queue
+// where, promises are attached to micro task queue and timeouts are
+// attached to callback queue.
+// execution order is micro task >>>>>> callback queue
