811a74c4c13fdcd94f4b437f0233357ff8dd65dd
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-04-14 14:22:29 +0530

    made a change

--- Call_Apply_Bind/ExampleThree.js
+++ Call_Apply_Bind/ExampleThree.js
@@ -4,10 +4,15 @@ setTimeout(() => {
     const status = "😊"
     var Obj = {
         status: "😂",
-        getStatus:  ()=> {
+        getStatus: function () {
             console.log(this.status)
         }
     }
     Obj.getStatus()
+    // Here, we are passing global this to getStatus function
+    // this never points to a function for eg:setTimeOut
+    // this either points to global window object or to the Object it belongs
+
     Obj.getStatus.call(this)
 }, 0)
+// Run in browser, in VS code output might defer
