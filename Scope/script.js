96b8c8c81f5949dcf4fd5852f8b5d956145174b7
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-04-26 15:22:34 +0530

    Made a change

---
+++ Scope/script.js
@@ -0,0 +1,12 @@
+// debugger
+const user = "Gajanan"
+const age = 22
+var x = 10
+let c=10
+function Add() {
+    const x = 8;
+    const y = 5;
+    console.log(x + y)
+}
+Add()
+console.log("Program ended")
